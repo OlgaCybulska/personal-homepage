2ea228b597a0dbb74ad9e3173700ce958b94cc12
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-30 20:48:05 +0100

    code formatted

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
-  font-family: 'Courier Prime', monospace;
   font-family: 'Inter', sans-serif;
 
  ;
