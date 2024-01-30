760cda7a48f329c16611a98df1978cf292eb0c74
Olga Cybulska <olgacybulska7@wp.pl>
2024-01-30 19:32:44 +0100

    created GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+  font-family: 'Courier Prime', monospace;
+font-family: 'Inter', sans-serif;
+
+ ;
+}
+
+*, ::after, ::befor {
+  box-sizing: inherit;
+}`;
