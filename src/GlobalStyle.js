fd05fba51dd9f21c3f8f533bb652a5703edc2af5
Olga Cybulska <olgacybulska7@wp.pl>
2024-02-03 22:31:23 +0100

    #root added

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -10,4 +10,14 @@ html {
 
 *, ::after, ::befor {
   box-sizing: inherit;
-}`;
+}
+#root {
+  width: 100%;
+  background-color: hsl(0, 0%, 95%);
+  margin: auto;
+}
+
+body {
+    margin:  auto;
+}
+`;
