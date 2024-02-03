eba81d0c154276cf41fbeaf08365a9563b97cfaa
Olga Cybulska <olgacybulska7@wp.pl>
2024-02-03 22:32:01 +0100

    Provider and GlobalStyle added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
+import { GlobalStyle } from "./GlobalStyle";
+import App from "../src/core/App";
+import store from "./core/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
