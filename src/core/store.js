4493338b1b1e8caa57734bdf20db501cd7f70f84
Olga Cybulska <olgacybulska7@wp.pl>
2024-02-09 15:46:59 +0100

    themeReducer added

--- src/core/store.js
+++ src/core/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./saga";
 import personalHomepageReducer from "../features/personal homepage/PersonalHomepage/personalHomepageSlice";
+import themeReducer from "../common/ThemeSwitch/themeSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     personalHomepage: personalHomepageReducer,
+    theme: themeReducer,
   },
   middleware: () => [sagaMiddleware],
 });
