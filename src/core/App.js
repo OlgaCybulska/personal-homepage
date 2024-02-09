f5fc7aaa336d1ff4e7f3f7ba3a5377ecca127e5c
Olga Cybulska <olgacybulska7@wp.pl>
2024-02-09 15:46:02 +0100

    GlobalStyle, ThemeProvider, and functions to App transferred

--- src/core/App.js
+++ src/core/App.js
@@ -1,6 +1,23 @@
 import PersonalHomepage from "../features/personal homepage/PersonalHomepage";
+import { Header } from "../features/personal homepage/Header";
+import { ThemeProvider } from "styled-components";
+import { selectIsDarkTheme } from "../common/ThemeSwitch/themeSlice";
+import { themeDark, themeLight } from "../common/theme";
+import { useSelector } from "react-redux";
+import Skillset from "../features/personal homepage/Skillset";
+import WantToLearn from "../features/personal homepage/WantoToLearn";
+
 function App() {
-  return <PersonalHomepage />;
+  const theme = useSelector(selectIsDarkTheme);
+
+  return (
+    <ThemeProvider theme={theme ? themeDark : themeLight}>
+      <Header />
+      <Skillset />
+      <WantToLearn />
+      <PersonalHomepage />
+    </ThemeProvider>
+  );
 }
 
 export default App;
