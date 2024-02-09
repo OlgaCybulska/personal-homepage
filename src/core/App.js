85ced93f03999ed6334115108745357b96fe1fb1
Olga Cybulska <olgacybulska7@wp.pl>
2024-02-09 15:51:06 +0100

    Globalstyle transferred

--- src/core/App.js
+++ src/core/App.js
@@ -6,12 +6,14 @@ import { themeDark, themeLight } from "../common/theme";
 import { useSelector } from "react-redux";
 import Skillset from "../features/personal homepage/Skillset";
 import WantToLearn from "../features/personal homepage/WantoToLearn";
+import { GlobalStyle } from "../GlobalStyle";
 
 function App() {
   const theme = useSelector(selectIsDarkTheme);
 
   return (
     <ThemeProvider theme={theme ? themeDark : themeLight}>
+      <GlobalStyle />
       <Header />
       <Skillset />
       <WantToLearn />
