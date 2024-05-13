558a125029a2e5e98fa6609d7f1d42ebabdf91c4
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-13 16:16:30 +0200

    style to StyledList and ListItem added

--- src/common/List/styled.js
+++ src/common/List/styled.js
@@ -1,5 +1,28 @@
 import styled from "styled-components";
+import { ReactComponent as Dot } from "../../images/dot.svg";
 
-export const StyledList = styled.div``;
+export const StyledList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  column-gap: 115px;
+  gap: 8px;
 
-export const ListItem = styled.li``;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-template-columns: repeat(1, auto);
+  }
+  @media (min-width: ${({ theme }) =>
+      theme.breakpoint.mobile}) and (max-width: ${({ theme }) =>
+      theme.breakpoint.tablet}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+export const ListItem = styled.li`
+  list-style-type: none;
+`;
+
+export const Dotlist = styled(Dot)`
+  width: 9px;
+  height: 9px;
+  margin-right: 16px;
+`;
