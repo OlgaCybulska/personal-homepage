063ba5048d318a4408ff68168f9941449a02c80e
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-13 16:18:17 +0200

    style to Text added

--- src/common/ThemeSwitch/styled.js
+++ src/common/ThemeSwitch/styled.js
@@ -5,23 +5,24 @@ import { ReactComponent as SunIcon } from "../../images/sun.svg";
 export const Wrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  background-color: ${({ theme }) => theme.background};
   grid-column-start: 1;
   grid-column-end: span 3;
 `;
 
 export const Button = styled.button`
-  border: solid;
+  border: none;
+  background-color: ${({ theme }) => theme.bodyBackground};
+  display: flex;
+  align-items: center;
 `;
 
-export const IconBox = styled.div`
-  ${({ movetoright }) =>
-    movetoright &&
-    css`
-      background-color: red;
-    `}
-`;
+export const IconBox = styled.div``;
 
 export const Icon = styled(SunIcon)``;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+  font-weight: 700;
+  color: ${({ theme }) => theme.text};
+  text-transform: uppercase;
+  margin-right: 12px;
+`;
