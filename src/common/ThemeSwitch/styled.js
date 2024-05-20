7458736b0395c95312dc545283e4961274c10b4d
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-20 14:11:39 +0200

    style added

--- src/common/ThemeSwitch/styled.js
+++ src/common/ThemeSwitch/styled.js
@@ -7,22 +7,64 @@ export const Wrapper = styled.div`
   justify-content: flex-end;
   grid-column-start: 1;
   grid-column-end: span 3;
+  margin: 0;
 `;
 
 export const Button = styled.button`
   border: none;
-  background-color: ${({ theme }) => theme.bodyBackground};
+  background-color: ${({ theme }) => theme.colors.bodyBackground};
   display: flex;
   align-items: center;
+  cursor: pointer;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    margin-right: 17px;
+  }
 `;
 
-export const IconBox = styled.div``;
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  height: 26px;
+  width: 48px;
+  padding: 3px;
+  border-radius: 30px;
+  transition: 0.3s;
+  cursor: pointer;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors.switchButtonBorder};
+  background-color: ${({ theme }) => theme.colors.switchButtonBackground};
+`;
 
-export const Icon = styled(SunIcon)``;
+export const IconBox = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.sunBox};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2px;
+
+  ${({ movetoright }) =>
+    movetoright &&
+    css`
+      transform: translateX(20px);
+    `}
+`;
+
+export const Icon = styled(SunIcon)`
+  fill: ${({ theme }) => theme.colors.sun};
+`;
 
 export const Text = styled.p`
   font-weight: 700;
-  color: ${({ theme }) => theme.text};
+  color: ${({ theme }) => theme.colors.text};
   text-transform: uppercase;
   margin-right: 12px;
+  font-size: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    display: none;
+  }
 `;
