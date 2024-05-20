0c4fa5e803c55f1d43bda332c092559a21a75f4e
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-20 14:11:49 +0200

    style added

--- src/features/personal homepage/Button/index.js
+++ src/features/personal homepage/Button/index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export default styled.button`
   text-decoration: none;
-  background-color: teal;
+  background-color: ${({ theme }) => theme.colors.buttonBackground};
   width: fit-content;
   border: 1px;
   padding: 12px 16px;
@@ -12,5 +12,5 @@ export default styled.button`
   display: flex;
   justify-content: center;
   gap: 16px;
-  color: ${({ theme }) => theme.buttonText};
+  color: ${({ theme }) => theme.colors.buttonText};
 `;
