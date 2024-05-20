f037c5e5a9e673a6d8826cdb811b4f730666fc75
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-20 14:10:38 +0200

    style added

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,21 +1,35 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.section`
-  background-color: ${({ theme }) => theme.sectionBackground};
+  background-color: ${({ theme }) => theme.colors.sectionBackground};
   margin-bottom: 72px;
   padding: 32px;
   border-radius: 4px;
-  box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
-    0px -2px 50px 0px rgba(9, 10, 51, 0.02);
+  box-shadow: 0px 16px 58px 0px ${({ theme }) => theme.colors.boxShadow},
+    0px -2px 50px 0px ${({ theme }) => theme.colors.boxShadow2};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    margin-top: ;
+    margin: 48px 14px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    padding: 16px;
+    margin-bottom: 50px;
+    gap: 12px;
+  }
 `;
 
 export const SectionHeader = styled.h2`
-  border-bottom: 1px solid ${({ theme }) => theme.underline};
-  padding-bottom: 15px;
-  margin: 0px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.underline};
+  padding-bottom: 16px;
+  margin: 0;
   font-weight: 900;
   font-size: 30px;
-  color: ${({ theme }) => theme.header};
+  line-height: 36.61px;
+  color: ${({ theme }) => theme.colors.header};
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  margin: 0;
+`;
