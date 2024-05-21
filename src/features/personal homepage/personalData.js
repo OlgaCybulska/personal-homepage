b17d69ad0f47e9cd3acae7f851e1302fe49abdba
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-21 15:30:52 +0200

    deleted unnecessary data

--- src/features/personal homepage/personalData.js
+++ src/features/personal homepage/personalData.js
@@ -1,13 +1,3 @@
-export const personalData = {
-  name: "Olga Cybulska",
-  github: "https://github.com/OlgaCybulska",
-  facebook: "https://www.facebook.com/olga.cybulska.7",
-  instagram: "",
-  linkedin: "",
-
-  currentSkills: [],
-  futureSkills: [],
-};
 export const email = "cybulskaolga02@gmail.com";
 
 export const getRepoFromApi = "https://api.github.com/users/OlgaCybulska/repos";
