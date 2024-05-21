105dc8df12fbe211544872e87eddf75668cc9653
Olga Cybulska <olgacybulska7@wp.pl>
2024-05-21 15:32:55 +0200

    animation added

--- src/features/personal homepage/PersonalHomepage/Content/Loading/styled.js
+++ src/features/personal homepage/PersonalHomepage/Content/Loading/styled.js
@@ -4,6 +4,14 @@ import { ReactComponent as LoadingIcon } from "../../../../../images/loading.svg
 export const StyledLoadingIcon = styled(LoadingIcon)`
   width: 160px;
   height: 160px;
+
+  animation: rotate infinite linear 1s;
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 export const Wrapper = styled.div`
