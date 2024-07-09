59060060fdf0e3d7349e5e07fda3387f886868e2
Olga Cybulska <olgacybulska7@wp.pl>
2024-07-09 17:43:24 +0200

    added style to the body

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,27 +3,27 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
-  font-family: 'Inter', sans-serif;
+  
 }
 
 *, ::after, ::befor {
   box-sizing: inherit;
 }
-#root {
-  width: 100%;
-  max-width: 1216px;
-  margin-left: 353px;
-  margin-top: 119px;
- 
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
-    margin: 16px;
 
-  }
-}
 
 body {
+  width: 100%;
+  max-width: 1216px;
   background-color: ${({ theme }) => theme.colors.bodyBackground};
-  transition: 0.3s;
+  transition: background  0.3s;
+  font-family: 'Inter', sans-serif;
+  word-break: break-word;
+  padding-bottom: 108px;
+  margin: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    padding-bottom: 32px;
+  }
  
 }
 `;
